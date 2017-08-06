38774400513ef248f0fb5f2c3c29f2edf03e041a
Jazende <smirkloph@gmail.com>
2017-08-06 17:33:11 +0200

    Update and rename problems_28.py to problems_33.py

--- problems_33.py
+++ problems_33.py
@@ -1,15 +1,3 @@
-def problem_28(target=1001):
-    start = 1
-    for x in range(2, target+1):
-        if x%2 == 1:
-            start += x**2
-            start += x**2 - (x-1)
-            start += x**2 - (x-1)*2
-            start += x**2 - (x-1)*3
-            print("x: ", x, "\t\t", x**2, x**2 - (x-1), x**2 - (x-1)*2, x**2 - (x-1)*3)
-    print(start)
-    return start
-
 def problem_33():
     num = [x for x in range(11, 101) if not (x%10 == 0) and not str(x)[0] == str(x)[1]]
     den = [x for x in range(11, 101) if not (x%10 == 0) and not str(x)[0] == str(x)[1]]
